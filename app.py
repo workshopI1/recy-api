f6cf24c0bddb8a6b63904b43bbcd8c22cc71a668
francois.desprez <francois.desprez@epsi.fr>
2020-11-18 09:42:34 +0100

    ajout post methods

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_wastes():
     with sql.connect('workshop.db') as con:
         con.row_factory = sql.Row
         cur = con.cursor()
-        rows = cur.execute("SELECT w.Id, w.name, m.name as mat FROM waste w CROSS JOIN material m on w.id_material = m.Id").fetchall()
+        rows = cur.execute("SELECT w.Id, w.name, m.Id as id_mat, m.name as mat FROM waste w CROSS JOIN material m on w.id_material = m.Id").fetchall()
         return json.dumps( [dict(row) for row in rows])
 
 @app.route('/recycling/get')
@@ -30,8 +30,24 @@ def get_materials():
     with sql.connect('workshop.db') as con:
         con.row_factory = sql.Row
         cur = con.cursor()
-        rows = cur.execute("SELECT m.Id, m.name, m.barCode, r.name as recycling FROM material m  CROSS JOIN recycling r on m.id_recycling = r.Id").fetchall()
+        rows = cur.execute("SELECT m.Id, m.name, m.barCode, r.Id as id_recycling, r.name as recycling FROM material m  CROSS JOIN recycling r on m.id_recycling = r.Id").fetchall()
         return json.dumps([dict(row) for row in rows])
 
+@app.route('/material/add')
+def add_material(name, barCode, id_recycling):
+    with sql.connect('workshop.db') as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO material (name, barCode, id_recycling) VALUES(?,?,?)", (name, barCode, id_recycling))
+        con.commit()
+        con.close()
+
+@app.route('/waste/add')
+def add_waste(name, id_material):
+    with sql.connect('workshop.db') as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO waste (name, id_material) VALUES(?,?)", (name,id_material))
+        con.commit()
+        con.close()
+        
 if __name__ == '__main__':
     app.run()
