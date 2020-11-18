e69a3d9131675ec05c1196f273b13cca2da8168e
francois.desprez <francois.desprez@epsi.fr>
2020-11-18 09:57:07 +0100

    finition post methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json
 import sqlite3 as sql
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
-@app.route('/waste/get')
+@app.route('/waste/get', methods=['GET'])
 def get_wastes():
     with sql.connect('workshop.db') as con:
         con.row_factory = sql.Row
@@ -17,7 +17,7 @@ def get_wastes():
         rows = cur.execute("SELECT w.Id, w.name, m.Id as id_mat, m.name as mat FROM waste w CROSS JOIN material m on w.id_material = m.Id").fetchall()
         return json.dumps( [dict(row) for row in rows])
 
-@app.route('/recycling/get')
+@app.route('/recycling/get', methods=['GET'])
 def get_recycling():
     with sql.connect('workshop.db') as con:
         con.row_factory = sql.Row
@@ -25,7 +25,7 @@ def get_recycling():
         rows = cur.execute("SELECT * FROM recycling").fetchall()
         return json.dumps([dict(row) for row in rows])
 
-@app.route('/material/get')
+@app.route('/material/get', methods=['GET'])
 def get_materials():
     with sql.connect('workshop.db') as con:
         con.row_factory = sql.Row
@@ -33,21 +33,23 @@ def get_materials():
         rows = cur.execute("SELECT m.Id, m.name, m.barCode, r.Id as id_recycling, r.name as recycling FROM material m  CROSS JOIN recycling r on m.id_recycling = r.Id").fetchall()
         return json.dumps([dict(row) for row in rows])
 
-@app.route('/material/add')
+@app.route('/material/add', methods=['POST'])
 def add_material(name, barCode, id_recycling):
     with sql.connect('workshop.db') as con:
         cur = con.cursor()
         cur.execute("INSERT INTO material (name, barCode, id_recycling) VALUES(?,?,?)", (name, barCode, id_recycling))
         con.commit()
-        con.close()
+        return ('row added')
 
-@app.route('/waste/add')
-def add_waste(name, id_material):
+@app.route('/waste/add', methods=['POST'])
+def add_waste():
     with sql.connect('workshop.db') as con:
+        name = request.args.get('name')
+        id_material = request.args.get('id_material')
         cur = con.cursor()
         cur.execute("INSERT INTO waste (name, id_material) VALUES(?,?)", (name,id_material))
         con.commit()
-        con.close()
-        
+        return ('row added')
+
 if __name__ == '__main__':
     app.run()
